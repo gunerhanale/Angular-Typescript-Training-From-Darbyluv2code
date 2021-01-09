6ee43e01225d3cb64ee08bfcd3a6bdbd70dd9ce3
gunerhanale <gunerhanale@gmail.com>
2021-01-09 14:04:09 +0100

    added how to create and use classes in Typescript

---
+++ typescript-training/04-classes/05-modules/Driver.ts
@@ -0,0 +1,7 @@
+import { Customer } from "./Customer";
+
+// let's create an instance
+let myCustomer = new Customer("Becry", "Gun");
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
